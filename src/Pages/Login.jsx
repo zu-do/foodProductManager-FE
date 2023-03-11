28a873f49b6396dfa93479d77c89dec3d015e5b9
rokman123 <rokismank@gmail.com>
2023-03-11 19:25:29 +0200

    Login and register possibility

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,35 @@
+import React, { useState } from "react";
 import "primereact/resources/primereact.min.css";
 import { InputText } from 'primereact/inputtext';
 import "../Styles/Register.css"
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import {loginUser} from "../Utils/user-axios-utils"
 
 
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const navigator = useNavigate();
     const navigateToMain = () => {
       navigator("/main");
     };
+
+    const onSubmit = (event) => {
+        event.preventDefault(); // prevent default form submit behavior
+    
+        const response = loginUser(email, password);
+        response.then((result) => {
+            if (result !== null){
+                sessionStorage.setItem("user", email);
+                navigateToMain();
+            }
+            else{
+                window.alert("Nepavyko prisijungti")
+            }
+        });
+      };
+
     return(
         <div className="ragisterPageFather">
             <div className="registerPageChild">
@@ -20,19 +40,23 @@ function Login() {
                     <div className="formDiv">
                         <div className="formComponent">
                             <label htmlFor="email" className="block text-900 font-medium mb-2 label">El. paštas</label>
-                            <InputText id="email" type="text" placeholder="El. paštas" className="w-full mb-3" />
+                            <InputText 
+                            id="email" 
+                            type="email" 
+                            placeholder="El. paštas" 
+                            className="w-full mb-3" 
+                            onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div className="formComponent"> 
                             <label htmlFor="password" className="block text-900 font-medium mb-2 label">Slaptažodis</label>
-                            <InputText type="password" placeholder="Slaptažodis" className="w-full mb-3" />
+                            <InputText
+                             type="password"
+                              placeholder="Slaptažodis" 
+                              className="w-full mb-3"
+                              onChange={(e) => setPassword(e.target.value)} />
                         </div>
                         <div className="formComponent"> 
-                            <button  className="registerButton" onClick={()=>
-                            {
-                                sessionStorage.setItem("user", "userEmail")
-                                navigateToMain();
-                        }}>Prisijungti</button>
-                            
+                            <button  className="registerButton" onClick={onSubmit}>Prisijungti</button>  
                         </div>
                     </div>
                     </form>
---
+++ src/Utils/user-axios-utils.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const client = axios.create({
+    baseURL: 'https://localhost:7258/User',
+  });
+
+  export const loginUser = async (email, password) => {
+    try {
+      const response = await client.get(`login/${email}/${password}`);
+      return response.data;
+    } catch (err) {
+      console.error(err.message);
+      return null;
+    }
+  };
+
+  export const registerUser = async (body) => {
+    try {
+      const response = await client.post(`register`, {...body});
+      return response.data;
+    } catch (err) {
+      console.log(err.message);
+      return null;
+    }
+  };
