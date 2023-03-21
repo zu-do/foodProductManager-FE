cfe16699af69a0922cd11cd2cac8e4a975ec6678
rokman123 <rokismank@gmail.com>
2023-03-21 18:16:39 +0200

    formating fix

--- src/Pages/UserOverview.jsx
+++ src/Pages/UserOverview.jsx
@@ -4,36 +4,30 @@ import { Column } from "primereact/column";
 import { getUsers } from "../Utils/user-axios-utils";
 
 function UserOverview() {
-   
-    useEffect(() => {
-        getUsers().then((data) => {
-            setUsers(data);
-        });
-      }, []);
-    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers().then((data) => {
+      setUsers(data);
+    });
+  }, []);
+  const [users, setUsers] = useState([]);
 
-    return(
-       <div>  
-
-
-       <DataTable
-          rowClassName="custom-row"
-          value={users}
-          tableStyle={{
-            width: "100%",
-            marginBottom: "20px",
-            borderRadius: "25px",
-            padding: "4rem"
-          }}
-        >
-          <Column field="name" header="Vartotojo vardas"></Column>
-          <Column field="lastname" header="Vartotojo pavardė"></Column>
-          <Column field="email" header="Vartotojo el. paštas"></Column>
-        
-
-        </DataTable>
-
-       </div>
-    )
+  return (
+    <div>
+      <DataTable
+        rowClassName="custom-row"
+        value={users}
+        tableStyle={{
+          width: "100%",
+          marginBottom: "20px",
+          borderRadius: "25px",
+          padding: "4rem",
+        }}
+      >
+        <Column field="name" header="Vartotojo vardas"></Column>
+        <Column field="lastname" header="Vartotojo pavardė"></Column>
+        <Column field="email" header="Vartotojo el. paštas"></Column>
+      </DataTable>
+    </div>
+  );
 }
 export default UserOverview;
